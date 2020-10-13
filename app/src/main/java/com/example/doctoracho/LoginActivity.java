12f5af4e50b9cd46f20c26b07d973bd61ed82587
Arifn Hossain <arifinhossain2000@gmail.com>
2020-10-14 02:07:42 +0600

    login& register activity inisitilize  done

--- app/src/main/java/com/example/doctoracho/LoginActivity.java
+++ app/src/main/java/com/example/doctoracho/LoginActivity.java
@@ -2,14 +2,44 @@ package com.example.doctoracho;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button button;
+    private EditText editmail, editpass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        textView = findViewById(R.id.regidterBtn);
+        button = findViewById(R.id.logBtn);
+        editmail = findViewById(R.id.logEmail);
+        editpass = findViewById(R.id.logPass);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/doctoracho/RegisterActivity.java
+++ app/src/main/java/com/example/doctoracho/RegisterActivity.java
@@ -2,13 +2,49 @@ package com.example.doctoracho;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+    private EditText regpass, regconpass, regmail, regName, regGender, regAge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        button = findViewById(R.id.regidterBtn);
+        textView = findViewById(R.id.haveBtn);
+        regAge  = findViewById(R.id.registerAge);
+        regpass = findViewById(R.id.registerPass);
+        regconpass = findViewById(R.id.registerConPass);
+        regmail = findViewById(R.id.registerEmail);
+        regName = findViewById(R.id.registerName);
+        regGender= findViewById(R.id.registerGender);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
