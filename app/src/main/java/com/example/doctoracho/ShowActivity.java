f1f9820da76445939b13f2378f3ac7d4260f06ad
Arifn Hossain <arifinhossain2000@gmail.com>
2020-10-14 12:58:03 +0600

    show xml, show activity, doctor_reg_xml done

--- app/src/main/java/com/example/doctoracho/ShowActivity.java
+++ app/src/main/java/com/example/doctoracho/ShowActivity.java
@@ -2,13 +2,59 @@ package com.example.doctoracho;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ShowActivity extends AppCompatActivity {
+
+    private ImageView admin, user, doctor;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
+
+        admin=findViewById(R.id.logAdmin);
+        user= findViewById(R.id.logUser);
+        doctor=findViewById(R.id.logDoctor);
+        textView=findViewById(R.id.regBtn);
+
+        admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ShowActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ShowActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        doctor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ShowActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ShowActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
