1b075c108a18eabdecddf41343960f499632bbc0
Arifn Hossain <arifinhossain2000@gmail.com>
2020-10-14 23:23:40 +0600

    Reg show and I have an account

--- app/src/main/java/com/example/doctoracho/HomeActivity.java
+++ app/src/main/java/com/example/doctoracho/HomeActivity.java
@@ -3,7 +3,6 @@ package com.example.doctoracho;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -45,7 +44,7 @@ public class HomeActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
 
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
+        inflater.inflate(R.menu.menu_item, menu);
         return true;
     }
 
@@ -53,6 +52,8 @@ public class HomeActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
         if(item.getItemId() == R.id.settingId){
+
+
 
         }
 
--- app/src/main/java/com/example/doctoracho/RegisterDoctor.java
+++ app/src/main/java/com/example/doctoracho/RegisterDoctor.java
@@ -2,13 +2,49 @@ package com.example.doctoracho;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RegisterDoctor extends AppCompatActivity {
+
+    private ImageView imageView;
+    private Button button;
+    private TextView textView2;
+    private EditText regpass, regconpass, regmail, regName, regGender, regAge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_doctor);
+
+        button = findViewById(R.id.regidterBtn);
+        textView2 = findViewById(R.id.haveBtn);
+        regAge  = findViewById(R.id.registerAge);
+        regpass = findViewById(R.id.registerPass);
+        regconpass = findViewById(R.id.registerConPass);
+        regmail = findViewById(R.id.registerEmail);
+        regName = findViewById(R.id.registerName);
+        regGender= findViewById(R.id.registerGender);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterDoctor.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        textView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterDoctor.this,ShowActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
